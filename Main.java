08b96abebba55f62139cb2c2c48fd6bb37ff0156
IbrahimAlKrad <70172217+IbrahimAlKrad@users.noreply.github.com>
2022-06-27 13:14:15 +0200

    Overloading a Mehtode

--- Main.java
+++ Main.java
@@ -5,10 +5,17 @@ public class Main {
         return min *60;
     }
 
+    //overloaded
+    public int minTOsec(int min, int sec) {
+
+        return (min*60 + sec);
+    }
+
     public static void main(String[] args) {
 
         Main main = new Main();
         System.out.println(main.minTOsec(60));
+        System.out.println(main.minTOsec(60, 30));
         
     }
 }
