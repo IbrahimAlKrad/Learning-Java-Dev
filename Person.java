67bf0a2e3e44bef52c606b93b543d96ea74e1ecf
IbrahimAlKrad <70172217+IbrahimAlKrad@users.noreply.github.com>
2022-07-02 23:59:26 +0200

    learning references and copies

--- Person.java
+++ Person.java
@@ -3,6 +3,20 @@ public class Person {
     private String firstName, lastName;
     private int age;
 
+    public Person(String firstName, String lastName, int age) {
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    //copy constructor
+    public Person(Person person) {
+
+        this.firstName = person.getFirstName();
+        this.lastName = person.getFirstName();
+        this.age = person.getAge();
+    }
     //getter
     public String getFirstName() {
         return firstName;
@@ -43,7 +57,15 @@ public class Person {
   }
 
   public static void main(String[] args) {
+
+    Person person = new Person("Max", "Mustermann", 23);
+    Person anotherPerson = new Person(person);
     
-  }
+    anotherPerson.setFirstName("John");
+    
+    System.out.println(anotherPerson.getFirstName());
+    System.out.println(person.getFirstName());
+    
+}
     
 }
