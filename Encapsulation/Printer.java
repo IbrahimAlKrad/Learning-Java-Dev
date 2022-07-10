f2f3025a6e98ad4dad399bbfad3e4599411047fa
IbrahimAlKrad <70172217+IbrahimAlKrad@users.noreply.github.com>
2022-07-10 13:38:27 +0200

    encapusulation

--- Encapsulation/Printer.java
+++ Encapsulation/Printer.java
@@ -5,30 +5,47 @@ public class Printer {
     private boolean duplex;
 
     public Printer(int tonerLevel, boolean duplex) {
-        this.tonerLevel = (tonerLevel >= 0 && tonerLevel <= 100) ? tonerLevel:0;
+        if (tonerLevel > -1 && tonerLevel <= 100) {
+            this.tonerLevel = tonerLevel;
+        } else {
+            this.tonerLevel = -1;
+        }
         this.duplex = duplex;
         this.pagesPrinted = 0;
     }
 
     public int addToner(int tonerAmount) {
-        if (0 <= tonerAmount && tonerAmount <= 100) {
-            tonerLevel = (tonerAmount + tonerLevel > 100) ? -1 : tonerLevel + tonerAmount;
+        if (0 < tonerAmount && tonerAmount <= 100) {
+            if (tonerAmount + this.tonerLevel > 100){
+                return -1;
+            } 
+      
+            this.tonerLevel += tonerAmount;
+            return this.tonerLevel;
+        
+        } else {
+            return -1;
         }
-        return tonerLevel;
     }
 
-    //ToDo
     public int printPages(int pages) {
         int pagesToPrint = pages;
 
         if (this.duplex) {
-
-            pagesPrinted += pagesToPrint;
+            pagesToPrint = (pages /2) + (pages % 2);
         }
+        pagesPrinted += pagesToPrint;
         return pagesToPrint;
     }
 
-    public boolean getPagesPrinted() {
+    public int getPagesPrinted() {
         return pagesPrinted;
+    }
+
+    public static void main(String[] args) {
+        Printer printer = new Printer(50, true);
+        System.out.println(printer.addToner(25));
+        System.out.println(printer.printPages(4));
+        System.out.println(printer.getPagesPrinted());
     }
 }
